e261d354c7017b7ab5dd350c1ff8c1774d38531d
TVW23 <200715@student.aventus.nl>
2024-04-25 14:10:59 +0200

    Getal raden is klaar

--- ThijsScherm.py
+++ ThijsScherm.py
@@ -1,9 +1,40 @@
 import tkinter as tk
-window = tk.Tk()
-greeting = tk.Label(text="Hey! Wat is jouw naam?")
+from tkinter import messagebox
+import random
 
-entry = tk.Entry(fg="blue", bg="white", width=50)
-greeting.pack()
-entry.pack()
+# Hier wordt de titel en de grootte van het schermpje.
+class Raadspel:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Raadspel")
+        self.master.geometry("300x150")
+        # Hier wordt een willekeurig getal gegenereerd 
 
-window.mainloop()
+        self.geheim_nummer = random.randint(1, 10)
+        self.raad_label = tk.Label(self.master, text="Voer je gok in:")
+        self.raad_label.pack()
+
+        self.raad_entry = tk.Entry(self.master)
+        self.raad_entry.pack()
+
+        self.submit_button = tk.Button(self.master, text="Indienen", command=self.controleer_gok)
+        self.submit_button.pack()
+
+    def controleer_gok(self):
+        gok = int(self.raad_entry.get())
+
+        if gok == self.geheim_nummer:
+            messagebox.showinfo("Gefeliciteerd!", "Je hebt het goed geraden!")
+            self.master.destroy()
+        elif gok < self.geheim_nummer:
+            messagebox.showinfo("Incorrect", "Probeer een hoger nummer!")
+        else:
+            messagebox.showinfo("Incorrect", "Probeer een lager nummer!")
+
+def main():
+    root = tk.Tk()
+    spel = Raadspel(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
