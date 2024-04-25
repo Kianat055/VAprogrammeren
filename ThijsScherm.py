eb7e363b794fcea2b519de4690d6e0ea33397377
TVW23 <200715@student.aventus.nl>
2024-04-25 13:15:16 +0200

    Tkinter

--- ThijsScherm.py
+++ ThijsScherm.py
@@ -1,1 +1,9 @@
-Thijs
+import tkinter as tk
+window = tk.Tk()
+greeting = tk.Label(text="Hey! Wat is jouw naam?")
+
+entry = tk.Entry(fg="blue", bg="white", width=50)
+greeting.pack()
+entry.pack()
+
+window.mainloop()
